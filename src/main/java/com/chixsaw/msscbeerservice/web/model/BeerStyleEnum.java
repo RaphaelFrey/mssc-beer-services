a6a927dcfd35eb7c507eb860a8a8c61e482c10d4
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-11-24 20:07:01 +0100

    adding web data model. closes #2

---
+++ src/main/java/com/chixsaw/msscbeerservice/web/model/BeerStyleEnum.java
@@ -0,0 +1,8 @@
+package com.chixsaw.msscbeerservice.web.model;
+
+/**
+ * Created by Raphael Frey on 24.11.2022
+ */
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
