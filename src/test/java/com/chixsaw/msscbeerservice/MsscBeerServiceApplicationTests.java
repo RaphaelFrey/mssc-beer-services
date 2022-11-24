6cf6c087df97837fb485db948ba12e33cd551f28
RaphaelFrey <raphael.marius.frey@gmail.com>
2022-11-24 19:52:52 +0100

    Initial commit

---
+++ src/test/java/com/chixsaw/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chixsaw.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
